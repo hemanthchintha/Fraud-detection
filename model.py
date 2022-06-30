e197b4aa8e77f6948df45adcb37ad38acfc7d756
sschintha <hemanthss21@gmail.com>
2022-06-30 05:43:23 +0530

    added 3 new model code

--- model.py
+++ model.py
@@ -1,1 +1,8 @@
 print("adding all mk related codes")
+
+def model1():
+    print("addng code for ml model 1")
+def model2():
+    print("addng code for ml model 2")    
+def model3():
+    print("addng code for ml model 3")    
